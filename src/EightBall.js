ff950c4f0e4e1d3102805731e89bc3547c887295
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2021-04-06 11:07:29 -0700

    finished main part

---
+++ src/EightBall.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+import './EightBall.css';
+
+function EightBall(props) {
+  const [color, setColor] = useState('black');
+  const [text, setText] = useState('Think of a Question')
+
+  function clickHandler() {
+    let {msg, color} = props.answers[Math.floor(Math.random() * props.answers.length)];
+    setColor(color);
+    setText(msg);
+  }
+
+  return (
+    <div className="EightBall-div"
+      style={{ backgroundColor: color }}
+      onClick={clickHandler}>
+      {text}
+    </div>
+  )
+}
+
+EightBall.defaultProps = {
+  answers: [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ]
+}
+
+export default EightBall;
